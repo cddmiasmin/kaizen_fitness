baa68df2b73efe0bc9d5348abacbf1bd49aec666
cddmiasmin <iasmin.dias87@gmail.com>
2023-09-17 14:38:13 -0300

    :on: React Navigation II

--- kaizen-fitness/App.js
+++ kaizen-fitness/App.js
@@ -1,15 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import Home from './src/screens/Home';
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
-    </Stack.Navigator>
-</NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
