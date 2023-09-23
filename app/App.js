e3adb4fdf1397ec704873f6675b91756678e5f2e
cddmiasmin <iasmin.dias87@gmail.com>
2023-09-23 18:24:26 -0300

    :iphone: Add UserType + create Home

--- app/App.js
+++ app/App.js
@@ -3,11 +3,13 @@ import React, { useState, useEffect, useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import Home     from './src/screens/Home';
 import SignIn   from './src/screens/SignIn';
+import SignUp   from './src/screens/SignUp';
+import UserType from './src/screens/UserType';
 
 import { UserContextProvider } from './src/contexts/UserContext';
 import { ColorContextProvider } from './src/contexts/ColorContext';
-
 
 import 'expo-dev-client';
 
@@ -37,7 +39,10 @@ export default function App() {
       <ColorContextProvider>
         <NavigationContainer>
           <Stack.Navigator initialRouteName="SignIn">
+            <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
             <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }}/>
+            <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }}/>
+            <Stack.Screen name="UserType" component={UserType} options={{ headerShown: false }}/>
           </Stack.Navigator>
         </NavigationContainer>
       </ColorContextProvider>
