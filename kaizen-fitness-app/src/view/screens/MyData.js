2ce785d6c4cd9a36664f835b07aa98b697f0c53f
cddmiasmin <iasmin.dias87@gmail.com>
2023-10-04 02:30:34 -0300

    :woman_mechanic: Starting MyData Sterilization

--- kaizen-fitness-app/src/view/screens/MyData.js
+++ kaizen-fitness-app/src/view/screens/MyData.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { mainColor } from '../../colors/colors';
 import ProfessionalController from '../../controller/ProfessionalController';
 import { StatusBar } from 'expo-status-bar';
 import { createJsonObject } from '../../utils/createJsonObject';
+import { AntDesign } from '@expo/vector-icons';
+import { TextInput } from 'react-native-paper';
 
 export default function MyData() {
+  const [text, setText] = React.useState("");
 
  const professionalController = new ProfessionalController();
 
@@ -20,21 +23,35 @@ export default function MyData() {
  return (
     <View style={styles.container}>
         <StatusBar style='light'/>
-        <Text
-            onPress={() => update()}
-        >Iasmin</Text>
+        <View style={styles.header}>
+            <TouchableOpacity onPress={() => navigation.navigate('Profile')} >
+                <AntDesign name="left" size={20} color="white" />
+            </TouchableOpacity>
+            <Text style={styles.screen}>Meus Dados</Text>
+        </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: mainColor,
-      justifyContent: 'star',
+  container:{
+    flex: 1,
+    backgroundColor: mainColor,
+    paddingLeft: '5%',
+    paddingRight: '5%',
+  },
+  header: {
+      marginTop: 60,
+      justifyContent: 'flex-start',
       alignItems: 'center',
-      paddingLeft: '8%',
-      paddingRight: '8%',
-      paddingTop: '20%'
-    },
+      flexDirection: 'row',
+      marginBottom: 40,
+  },
+  screen: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 45
+  },
 });
