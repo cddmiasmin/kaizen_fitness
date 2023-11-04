c64a4ebb98beec5eae9061b55ea733fa591fe10e
cddmiasmin <iasmin.dias87@gmail.com>
2023-11-04 20:35:13 -0300

    :woman_mechanic: SignUp screen: New design

--- kaizen-fitness-app/App.js
+++ kaizen-fitness-app/App.js
@@ -39,7 +39,7 @@ export default function App() {
 
   const userController = new UserController();
 
-  const [route, setRoute] = useState('SignIn');
+  const [route, setRoute] = useState('SignUp');
   const [hasRegister, setHasRegister] = useState(false)
   const [initializing, setInitializing] = useState(true);
   const [userAux, setUserAux] = useState('');
--- kaizen-fitness-app/src/view/screens/SignUp.js
+++ kaizen-fitness-app/src/view/screens/SignUp.js
@@ -2,17 +2,16 @@ import { useContext, useState } from 'react';
 import { View, 
     StyleSheet, 
     Text, 
-    TouchableOpacity, 
-    TextInput,  
+    TouchableOpacity,   
     KeyboardAvoidingView,
-    ScrollView, 
+    Image,
 } from 'react-native';
+import { TextInput, Snackbar, IconButton, Avatar } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 
 import { StatusBar } from 'expo-status-bar';
-import { AntDesign } from '@expo/vector-icons';
 
-import { mainColor } from '../../colors/colors';
+import { error, grayText, mainColor, success } from '../../colors/colors';
 
 import { ColorContext } from '../../contexts/ColorContext';
 
@@ -20,165 +19,334 @@ import UserController from '../../controller/UserController';
 
 export default function SignUp() {
 
+    const [name, setName] = useState('Florence');
+    const [familyName, setFamilyName] = useState('Welch');
+    const [email, setEmail] = useState('florence.welch@hotmail.com');
+    const [password, setPassword] = useState('123456');
+    const [confirmPassword, setConfirmPassword] = useState('123456');
+
+    const [colorTextPassword, setColorTextPassword] = useState(color);
+    const [secureTextEntryForPassword, setSecureTextEntryForPassword] = useState(true);
+    const [colorTextConfirmPassword, setColorTextConfirmPassword] = useState(color);
+    const [secureTextEntryForConfirmPassword, setSecureTextEntryForConfirmPassword] = useState(true);
+
+    const [visibleSnackbar, setVisibleSnackbar] = useState(false);
+    const [messageSnackBar, setMessageSnackbar] = useState('iasmin');
+
+    const [signInResult, setSignInResult] = useState(true);
+  
     const navigation = useNavigation();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-
     const userController = new UserController();
-    
+  
     const {
       color
     } = useContext(ColorContext);
-  
-    return (
-    <ScrollView style={styles.scroll}>
-      <View style={styles.container}>
-        <StatusBar style='light' />
-        <View style={styles.constainerTexts}>
-          <Text style={styles.title}>Cadastro</Text>
-          <Text style={styles.subtitle}>Crie uma conta</Text>
-        </View>
-        <TouchableOpacity
-          onPress={() => userController.signInGoogle()}
-          style={[styles.google, { backgroundColor: color }]}>
-          <AntDesign name="google" size={20} color="white" />
-        </TouchableOpacity >
-        <Text style={styles.info}>ou cadastre-se com e-mail </Text>
-        <KeyboardAvoidingView style={styles.containerInput}>
-          <Text style={[styles.span, { color: color }]}>Email</Text>
-          <View style={styles.inputComponent}>
-            <TextInput
-                style={styles.input}
-                underlineColorAndroid="transparent"
-                onChangeText={(text) => setEmail(text)}
-                value={email}
-            />
-          </View>
-          <Text style={[styles.span, { color: color, marginTop: 20 }]}>Senha</Text>
-          <View style={styles.inputComponent}>
-            <TextInput
-                style={styles.input}
-                underlineColorAndroid="transparent"
-                onChangeText={(text) => setPassword(text)}
-                value={password}
-            />
-          </View>
 
-          <Text style={[styles.span, { color: color, marginTop: 20 }]}>Confirme a Senha</Text>
-          <View style={styles.inputComponent}>
-            <TextInput
-                style={styles.input}
-                underlineColorAndroid="transparent"
-                onChangeText={(text) => setConfirmPassword(text)}
-                value={confirmPassword}
-            />
+    const makeUserSignUp = async () => {
+      // const response = await userController.signIn(email, password);
+
+      // setSignInResult(response.result);
+      // setMessageSnackbar(response.message);
+      // setVisibleSnackbar(true)
+
+      // if(signInResult) navigation.navigate('Home');
+
+    }
+   
+    return (
+      <View style={styles.container}>
+        <StatusBar style="light" />
+          <View style={styles.header}>
+            <Text style={styles.headerTitle}>Cadastro</Text>
+            <View style={[styles.headerLine,{ backgroundColor: color }]} />
           </View>
-        </KeyboardAvoidingView>
-        <View style={styles.footer}>
-          <TouchableOpacity 
-              onPress={() => {
-                userController.signIn(email, password);
-                navigation.navigate('Register');
-              }} 
-              style={[styles.register, { backgroundColor: color }]}
+          <View style={styles.body}>
+          <View style={styles.containerPhoto}>
+              <View style={styles.avatar}>                    
+                  <IconButton
+                      icon="image-plus"
+                      iconColor={'white'}
+                      style={[{backgroundColor: color}, styles.icon]}
+                      size={15}
+                      onPress={() => console.log('Pressed')}
+                  />
+                  <Avatar.Image
+                      size={80} 
+                      source={{uri: 'https://i.pinimg.com/564x/25/fe/9f/25fe9f36701bdd3b29e4e1ed4dabe032.jpg'}}
+                      style={{zIndex: 1}}
+                  />
+              </View>
+            </View>
+            <KeyboardAvoidingView style={styles.keyboardArea}>
+              <View style={styles.containerTextInput}>
+                <View style={styles.textInput}>
+                  <Text style={{color: 'white', fontWeight: 'bold'}}>Nome</Text>
+                  <TextInput
+                    mode='flat'
+                    label=''
+                    value={name}
+                    onChangeText={(text) => setName(text)}
+                    underlineColor={'white'}
+                    activeUnderlineColor={color}
+                    textColor={grayText}
+                    editable={true}
+                    style={
+                      { backgroundColor: mainColor , width: 350, height: 35}
+                    }
+                    theme={{
+                      colors: {
+                          onSurfaceVariant: 'white'
+                      }
+                    }}
+                  />
+                </View>
+                <View style={styles.textInput}>
+                  <Text style={{color: 'white', fontWeight: 'bold'}}>Sobrenome</Text>
+                  <TextInput
+                    mode='flat'
+                    label=''
+                    value={familyName}
+                    onChangeText={(text) => setFamilyName(text)}
+                    underlineColor={'white'}
+                    activeUnderlineColor={color}
+                    textColor={grayText}
+                    editable={true}
+                    style={
+                      { backgroundColor: mainColor , width: 350, height: 35}
+                    }
+                    theme={{
+                      colors: {
+                          onSurfaceVariant: 'white'
+                      }
+                    }}
+                  />
+                </View>
+                <View style={styles.textInput}>
+                  <Text style={{color: 'white', fontWeight: 'bold'}}>Email</Text>
+                  <TextInput
+                    mode='flat'
+                    label=''
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                    underlineColor={'white'}
+                    activeUnderlineColor={color}
+                    textColor={grayText}
+                    editable={true}
+                    style={
+                      { backgroundColor: mainColor , width: 350, height: 35}
+                    }
+                    theme={{
+                      colors: {
+                          onSurfaceVariant: 'white'
+                      }
+                    }}
+                  />
+                </View>
+                <View style={styles.textInput}>
+                  <Text style={{color: 'white', fontWeight: 'bold'}}>Senha</Text>
+                  <TextInput
+                    mode='flat'
+                    label=''
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                    underlineColor={'white'}
+                    activeUnderlineColor={color}
+                    textColor={grayText}
+                    editable={true}
+                    style={
+                      { backgroundColor: mainColor , width: 350, height: 35}
+                    }
+                    theme={{
+                      colors: {
+                          onSurfaceVariant: 'white'
+                      }
+                    }}
+                    onFocus={() => setColorTextPassword(color)}
+                    onBlur={() => setColorTextPassword(grayText)}
+                    secureTextEntry={secureTextEntryForPassword}
+                    right={
+                      <TextInput.Icon 
+                        icon={secureTextEntryForPassword === true ? 'eye' : 'eye-off'} 
+                        color={colorTextPassword}
+                        onPress={() => setSecureTextEntryForPassword(!secureTextEntryForPassword)}
+                      />
+                    }
+                  />
+                </View>
+                <View style={styles.textInput}>
+                  <Text style={{color: 'white', fontWeight: 'bold'}}>Confirmar senha</Text>
+                  <TextInput
+                    mode='flat'
+                    label=''
+                    value={confirmPassword}
+                    onChangeText={(text) => setConfirmPassword(text)}
+                    underlineColor={'white'}
+                    activeUnderlineColor={color}
+                    textColor={grayText}
+                    editable={true}
+                    style={
+                      { backgroundColor: mainColor , width: 350, height: 35}
+                    }
+                    theme={{
+                      colors: {
+                          onSurfaceVariant: 'white'
+                      }
+                    }}
+                    onFocus={() => setColorTextConfirmPassword(color)}
+                    onBlur={() => setColorTextConfirmPassword(grayText)}
+                    secureTextEntry={secureTextEntryForConfirmPassword}
+                    right={
+                      <TextInput.Icon 
+                        icon={secureTextEntryForConfirmPassword === true ? 'eye' : 'eye-off'} 
+                        color={colorTextConfirmPassword}
+                        onPress={() => setSecureTextEntryForConfirmPassword(!secureTextEntryForConfirmPassword)}
+                      />
+                    }
+                  />
+                </View>
+              </View>
+              <TouchableOpacity 
+                style={[styles.signUp, { backgroundColor: color }]}
+                onPress={() => makeUserSignUp()}
+              >
+                <Text style={{color: 'white', fontWeight: 'bold'}}>Casdastrar</Text>
+              </TouchableOpacity>
+            </KeyboardAvoidingView>
+            <TouchableOpacity 
+                style={styles.newUser}
+                onPress={() => navigation.navigate('SignUp')}
+            >
+                <Text style={{color: grayText, fontWeight: 'normal'}}>Já tem uma conta?</Text>
+                <Text style={{color: color, fontWeight: 'bold'}}>Entre</Text>
+            </TouchableOpacity>
+            <View style={styles.continue}>
+              <Text style={{color: grayText, fontWeight: 'normal'}}>Ou continue com</Text>
+              <TouchableOpacity 
+                style={styles.google}
+                // onPress={() => setVisibleSnackbar(true)}
+              >
+                <Image
+                  style={{ width: 30, height: 30}} 
+                  source={require('../../assets/SignIn/icon_google-logo.png') }
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <Snackbar
+            visible={visibleSnackbar}
+            onDismiss={() => setVisibleSnackbar(false)}
+            duration={3000}
+            action={{
+              label: 'Ok',
+              textColor: signInResult === true ? 'white' : 'black',
+              onPress: () => {
+                setVisibleSnackbar(false);
+              },
+            }}
+            style={[styles.snackbar, signInResult === true ? styles.snackbarSucess : styles.snackbarError]}
           >
-            <Text style={styles.titleButton}>Cadastrar</Text>
-          </TouchableOpacity>
-          <Text style={styles.terms}>
-            Ao continuar, confirme que você concorda com nossos termos e condições
-          </Text>
-        </View>
+            <Text style={{ color: signInResult === true ? 'white' : 'black'}}>
+              {messageSnackBar}
+            </Text>
+          </Snackbar>
       </View>
-        </ScrollView>
-  );
+    )
 }
 
 const styles = StyleSheet.create({
-    scroll: {
-        flex: 1,
-        backgroundColor: mainColor,
-        paddingLeft: '8%',
-        paddingRight: '8%',
-        paddingTop: '5%',
-    },
     container: {
+      flex: 1,
+      backgroundColor: mainColor,
+      paddingLeft: '8%',
+      paddingRight: '8%',
+      paddingTop: '5%',
       justifyContent: 'star',
       alignItems: 'center',
     },
-    constainerTexts: {
+    header: {
+      marginTop: 25,
       justifyContent: 'center',
       alignItems: 'center',
+    },
+    headerTitle: {
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: 16,
+      marginBottom: 15
+    },
+    headerLine: {
+      width: 400,
+      height: 5
+    },
+    body: {
+      marginTop: 15,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    containerPhoto: {
+      width: '100%',
+      alignItems: 'center',
+    },
+    icon: {
+      position: 'absolute', 
+      top: '55%', 
+      left: '15%', 
+      zIndex: 2
+    },
+    avatar: {
+      zIndex: 1
+    },
+    keyboardArea:{
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    containerTextInput: {
+      width: '100%',
       flexDirection: 'column',
-      gap: 4,
-      marginTop: '10%',
+      gap: 20
     },
-    title: {
-      color: 'white',
-      fontSize: 32,
-      fontWeight: 'bold'
+    forgotPassword: {
+      marginTop: 15,
+      alignSelf: 'flex-start'
     },
-    subtitle: {
-      fontSize: 12,
-      color: 'white'
-    }, 
-    google: {
-    marginTop: '15%',
-    width: 50,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 50
-    },
-    info: {
-      color: 'white',
-      fontSize: 14,
-      marginTop: '10%',
-      marginBottom: '10%',
-      textAlign: 'center'
-    }, 
-    containerInput: {
-      width: '100%'
-    },
-    inputComponent: {
-      width: '100%',
-      height: 45,
-      backgroundColor: 'white',
+    signUp: {
+      width: 350,
+      height: 40,
+      marginTop: 30,
       justifyContent: 'center',
       alignItems: 'center',
-      borderRadius: 10
+      borderRadius: 5
     },
-    input: {
-      width: '95%',
-      height: 35,
-      backgroundColor: 'white',
-    },
-    span: {
-      fontWeight: 'bold',
-      marginBottom: 5
-    },
-    footer: {
-     marginTop: '30%',
-     justifyContent: 'center',
-     alignItems: 'center',
-     width: '100%'
-    },
-    register: {
-      width: '100%',
-      height: 50,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 15
-    },
-    titleButton: {
-      fontWeight: 'bold',
-      color: 'white',
-    },
-    terms: {
-      color: 'white',
-      textAlign: 'center',
+    newUser: {
       marginTop: 20,
-      fontSize: 10
+      flexDirection: 'row',
+      gap: 2.5
+    }, 
+    continue: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 25,
+      flexDirection: 'column',
+      gap: 15
+    },
+    google: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 45,
+      height: 45,
+      borderWidth: 1,
+      borderColor: 'white',
+      borderRadius: 5
+    },
+    snackbar: {
+      width: 350,
+      alignSelf: 'center',
+    },
+    snackbarSucess: {
+      backgroundColor: success,
+    },
+    snackbarError: {
+      backgroundColor: error,
     }
 });
